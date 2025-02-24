27bfa40e047d578b4f62004697f5e388d7cd08fd
Abdur-Rehman-code <rehman.abdur@devsinc.com>
2025-02-24 22:33:20 +0500

    updated code - added test for admin and models, and some custom functionallity in base admin class

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,46 @@
-from django.db import models    # noqa
+"""
+Database Model
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, #functionally for authetication
+    BaseUserManager,
+    PermissionsMixin  #functionallity for permissions,fields
+)
+
+
+class UsersManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self,email, password=None, **extra_field):
+        """create, save and return new user"""
+
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser"""
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User (AbstractBaseUser, PermissionsMixin):
+    """Users in the systemUSERNAME_FIELD."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UsersManager()
+    USERNAME_FIELD = 'email'  #field will be used for authenticaiton
